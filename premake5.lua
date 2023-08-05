3bc6c3725f03a5915bec19ecc73477aed3190a54
toma222 <aidannwilhite@gmail.com>
2023-08-05 15:59:25 +0000

    hi

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "gerald"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines
+    {
+        "SPDLOG_COMPILED_LIB"
+    }
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		runtime "Release"
+		optimize "on"
